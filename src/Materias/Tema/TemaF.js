4b1a5fe05f8f9184f683de4877647c8105dcf372
Axel <axosar@gmail.com>
2019-11-29 12:27:20 -0600

    Toggle function

--- src/Materias/Tema/TemaF.js
+++ src/Materias/Tema/TemaF.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
+import useToggle from './Hooks/useToggle';
 
 function Temaf() {
   const [count, setCount] = useState(0);
+  const [isHappy, toggleisHappy] = useToggle(true);
+  const [isHeartBroken, toggleisHeartBroken] = useToggle(false);
   // 1. the piece of state
   // 2. function that updates that piece of state
   return(
   <div>
     <h1>The count is: {count}</h1>
     <button onClick={() => setCount(count + 1)}>INCREMENT</button>
+    <h1 onClick={toggleisHappy}>{isHappy ? '😘' : '🤬'}</h1>
+    <h1 onClick={toggleisHeartBroken}>{isHeartBroken ? '💓' : '💔'}</h1>
   </div>
   )
   
